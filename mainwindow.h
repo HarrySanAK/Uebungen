92acff97017e202e3bfb63d29b1374ed05be6e3a
HarrySanAK <harryakcura@hotmail.com>
2025-05-09 09:06:44 +0200

    Update UI

--- mainwindow.h
+++ mainwindow.h
@@ -1,5 +1,7 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
+#include <QLabel>
+#include <QTimer>
 
 #include <QMainWindow>
 
@@ -16,6 +18,15 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+public:
+    QLabel * label_Bild;
+    QTimer *timet_bild_1,*timet_bild_2, *timet_bild_3, *timet_bild_4, *timet_bild_5;
+public slots:
+    void Timer_bild_1_Timeout_Event_Slot();
+    void Timer_bild_2_Timeout_Event_Slot();
+    void Timer_bild_3_Timeout_Event_Slot();
+    void Timer_bild_4_Timeout_Event_Slot();
+    void Timer_bild_5_Timeout_Event_Slot();
 
 private:
     Ui::MainWindow *ui;
